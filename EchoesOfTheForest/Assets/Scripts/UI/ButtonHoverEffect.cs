fe38e5a67514971ee9096499eadeb57d07b427f3
Allan Acosta <allanacost.27@gmail.com>
2024-08-23 02:26:30 -0500

    Adjustment the Main Menu - Player v2

---
+++ EchoesOfTheForest/Assets/Scripts/UI/ButtonHoverEffect.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TMP_Text buttonText; // Si usas TextMeshPro, cámbialo a TMP_Text
+    public Image underlineImage; // La línea que quieres mostrar
+    public Color hoverTextColor = Color.yellow; // El color que quieres cuando el mouse está sobre el botón
+    private Color originalTextColor;
+
+    void Start()
+    {
+        // Guardar el color original del texto
+        originalTextColor = buttonText.color;
+        // Asegúrate de que la línea esté oculta inicialmente
+        underlineImage.gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Cambiar el color del texto
+        buttonText.color = hoverTextColor;
+        // Mostrar la línea
+        underlineImage.gameObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Restaurar el color original del texto
+        buttonText.color = originalTextColor;
+        // Ocultar la línea
+        underlineImage.gameObject.SetActive(false);
+    }
+}
