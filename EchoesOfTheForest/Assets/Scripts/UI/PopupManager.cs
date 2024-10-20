d82a1b44d04e302aff8fd0f0fc307d2b2b95fc9f
Joseph Acosta <allanacost.27@gmail.com>
2024-10-20 03:05:16 -0500

    Add Popup game guide

---
+++ EchoesOfTheForest/Assets/Scripts/UI/PopupManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupManager : MonoBehaviour
+{
+    public GameObject popup;
+    public string animationName = "WarningText"; // El nombre de tu animación
+    private Animation popupAnimation;
+
+    void Start()
+    {
+        // Ocultamos el popup al inicio
+        popup.SetActive(false);
+
+        // Obtenemos el componente Animation del popup
+        popupAnimation = popup.GetComponent<Animation>();
+
+        // Programamos el popup para que aparezca después de 10 segundos
+        Invoke("ShowPopup", 8f);
+    }
+
+    void ShowPopup()
+    {
+        // Activamos el popup y reproducimos la animación de aparición
+        popup.SetActive(true);
+        popupAnimation.Play(animationName);
+
+        // Programamos que se oculte después de 7 segundos
+        Invoke("HidePopup", 9f);
+    }
+
+        void HidePopup()
+    {
+        // Reproducimos la misma animación pero en reversa
+        StartCoroutine(PlayAnimationReverse(animationName));
+    }
+
+        IEnumerator PlayAnimationReverse(string animName)
+    {
+        // Obtenemos la animación
+        AnimationState animState = popupAnimation[animName];
+
+        // Empezamos desde el final de la animación
+        animState.time = animState.length;
+        animState.speed = -1;  // Velocidad negativa para reproducir en reversa
+
+        // Reproducimos la animación
+        popupAnimation.Play(animName);
+
+        // Esperamos a que la animación en reversa termine
+        yield return new WaitForSeconds(animState.length);
+
+        // Desactivamos el popup
+        popup.SetActive(false);
+
+    }
+}
+
